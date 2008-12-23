3f1eadd7433b85bc92f967fff4a84e74dc6ecfba
veneficusunus <veneficusunus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-23 14:10:43 +0000

    Updated Plugin URI. It now points to the post about the plugin on return-true.com.
    git-svn-id: https://plugins.svn.wordpress.org/the-attached-image/trunk@82060 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- the-attached-image.php
+++ the-attached-image.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: The Attached Image
-Plugin URI: http://return-true.com
+Plugin URI: http://return-true.com/wordpress-plugin-the_attached_image/375/
 Description: Display the first image attached to a post. Use in the post loop. Order can be changed using menu order via the WP gallery. Based on the post image WordPress plugin by Kaf Oseo.
 Version: 1.3
 Author: Paul Robinson
