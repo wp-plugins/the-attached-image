f09254d1e92d2a79271f7c2d57dc6f3b148a9bc3
veneficusunus <veneficusunus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-24 16:04:46 +0000

    Archiving version 1.3 ready for the release of 1.4.
    git-svn-id: https://plugins.svn.wordpress.org/the-attached-image/tags/1.3@82306 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== The Attached Image ===
+Contributors: veneficusunus
+Donate link: http://return-true.com/want-to-help-return-true/
+Tags: images, attachments, posts
+Requires at least: 2.5
+Tested up to: 2.7
+Stable tag: 1.3
+
+This plugin takes the first menu ordered image that is attached to a post & echo/returns a URL or HTML img tag.
+
+== Description ==
+
+The idea behind this plugin is to show the first image attached to a post. The image that is shown can be customized as the image shown is always the one marked as first in the WordPress gallery order. You can supply a custom CSS class and it will be placed in the img tag. You can also switch between outputing a URL or an full img tag. Finally you can choose whether to echo or return the img or URL.
+
+Support for using a default image has now been added since version 1.3. It can be defined in the template & then over-rode using the custom fields on any post you like.
+
+== Installation ==
+
+1. Unzip the zip file.
+1. place the file 'the_attached_image.php' in the 'wp-content/plugins' folder.
+1. Place `<?php the_attached_image(); ?>` in your template.
+
+== Frequently Asked Questions ==
+
+= Can I use it outside the loop? =
+
+No, sorry. It uses some WP functions that only run in the loop.
+
+= What about a post link? =
+
+The most recent version of the_attached_image() has the added ability to surround the img tag with a href to link to the post, picture(default) or the attachment page.
+
+= What about setting a default image? =
+
+Indeed you can. As of version 1.3 you can now set a default image to be shown if there are no images attached to a post. You must use a domainless absolute path such as `/wp-content/default_images/default.jpg`. The call would look something like `<?php the_attached_image('default=/wp-content/default_images/default.jpg') ?>`. If you want to use a different default image for certain posts then you can set a different one using the custom fields. The key would be `default_pic` and the values would again be a domainless path.
+
+== Screenshots ==
+
+1. An example of what can be achived using The Attached Image plugin.
---
+++ the-attached-image.php
@@ -0,0 +1,125 @@
+<?php
+/*
+Plugin Name: The Attached Image
+Plugin URI: http://return-true.com/wordpress-plugin-the_attached_image/375/
+Description: Display the first image attached to a post. Use in the post loop. Order can be changed using menu order via the WP gallery. Based on the post image WordPress plugin by Kaf Oseo.
+Version: 1.3
+Author: Paul Robinson
+Author URI: http://return-true.com
+
+	Copyright (c) 2008, 2009 Paul Robinson (http://return-true.com)
+	The Attached Image is released under the GNU General Public License (GPL)
+	http://www.gnu.org/licenses/gpl.txt
+
+	This is a WordPress 2 plugin (http://wordpress.org). Based on the post image WordPress plugin by Kaf Oseo.
+	
+~Changelog:
+V 1.3:
+Added ability to choose a default image to show
+should there be no image to display. If no default 
+image is defined then nothing will be returned.
+Default image can be defined in the post via
+custom fields using 'default_pic' as the key.
+
+~~~~~~~~~~~~~~~~~~~~~
+
+V 1.2 ~ 75 - 90:
+Added the ability to output a link around the image.
+You can choose to link to the pic (default), the
+attachment page or the post that the image is attached to.
+
+~~~~~~~~~~~~~~~~~~~~~
+
+V 1.1 ~ line 62 & 68:
+Fixed a stupid mistake. Used strict type check for true.
+Not going to work if the true is provided by argument
+as it will be a string not a bool.
+*/
+
+function the_attached_image($args='') {
+	global $post;
+		
+	parse_str($args);
+	
+	if( !isset($img_size) ) $img_size = 'thumb';
+	if( !isset($css_class) ) $css_class = 'attached-image';
+	if( !isset($img_tag) ) $img_tag = true;
+	if( !isset($echo) ) $echo = true;
+	if( !isset($href) ) $href = false;
+	if( !isset($link) ) $link = 'pic';
+	if( !isset($default) ) $default = false;
+	
+	if(empty($post))
+		return;
+	
+	$attachments = get_children("post_parent=".$post->ID."&post_type=attachment&post_mime_type=image&numberposts=1&orderby=menu_order&order=ASC");
+	
+	if(empty($attachments)) {
+		if($pic_meta = get_post_meta($post->ID, 'default_pic', true)) {
+			$default_info = @getimagesize($default);
+			$default = $pic_meta;
+		} elseif($default === false) {
+			return;
+		}
+		
+		$image = '<img src="'.get_bloginfo('url').$default.'" class="'.$css_class.'" '.$default_info[3].' />';
+		
+		if($echo === true || $echo == 'true') {
+			echo $image;
+			return;
+		} else {
+			return $image;
+		}
+	}
+		
+	foreach($attachments as $attachment) {
+		$attachment = $attachment;
+		break;
+	}
+
+	$img_url = wp_get_attachment_url($attachment->ID);
+	
+	if ( $img_size == 'medium' ) {
+		if ( $intermediate = image_get_intermediate_size($attachment->ID, $img_size) ) {
+			$img_url = str_replace(basename($img_url), $intermediate['file'], $img_url);
+			$width = $intermediate['width'];
+			$height = $intermediate['height'];
+		}
+	} else {
+		if ( $intermediate = image_get_intermediate_size($attachment->ID, 'thumbnail') ) {
+			$img_url = str_replace(basename($img_url), $intermediate['file'], $img_url);
+			$width = $intermediate['width'];
+			$height = $intermediate['height'];
+		}
+	}
+	
+	if($img_tag === true || $img_tag == 'true') {	
+		$image = '<img src="'.$img_url.'" class="'.$css_class.'" title="'.$attachment->post_title.'" width="'.$width.'" height="'.$height.'" />';
+	} else {
+		$image = $img_url;
+	}
+	
+	if($href === true || $href == 'true') {
+		switch ($link) {
+			case 'post' :
+				$a_href = '<a href="'.get_permalink($post->ID).'" title="'.$post->post_title.'">%%%</a>';
+			break;
+			case 'attachment' :
+				$a_href = '<a href="'.get_attachment_link($attachment->ID).'" title="'.$attachment->post_title.'">%%%</a>';
+			break;
+			default :
+				$a_href = '<a href="'.wp_get_attachment_url($attachment->ID).'" title="'.$attachment->post_title.'">%%%</a>';
+			break;
+		}
+	}
+	if(isset($a_href) && !empty($a_href)) {
+		$image = str_replace('%%%', $image, $a_href);
+	}
+	
+	if($echo === true || $echo == 'true')
+		echo $image;
+	else
+		return $image;
+	
+}
+?>
