a745b45257e0269e1e2703a8348c2f09053a1826
veneficusunus <veneficusunus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-10 14:04:54 +0000

    Minor bug fix for default image custom fields.
    git-svn-id: https://plugins.svn.wordpress.org/the-attached-image/trunk@93365 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- the-attached-image.php
+++ the-attached-image.php
@@ -3,7 +3,7 @@
 Plugin Name: The Attached Image
 Plugin URI: http://return-true.com/wordpress-plugin-the_attached_image/375/
 Description: Display the first image attached to a post. Use the_attached_image() in the post loop. Order can be changed using menu order via the WP gallery. Based on the post image WordPress plugin by Kaf Oseo.
-Version: 2.1
+Version: 2.1.1
 Author: Paul Robinson
 Author URI: http://return-true.com
 
@@ -40,7 +40,7 @@ function the_attached_image($args='') {
 			$custom_img = false;	
 	}
 	
-	if(empty($post) && $custom_img === false) //If WP's post array is empty we can't do anything but only if a custom image hasn't been set.
+	if(empty($post) && $custom_img === false && get_post_meta($post->ID, 'att_default_pic', true) == "") //If WP's post array is empty we can't do anything but only if a custom image hasn't been set.
 		return false;
 	
 	if($custom_img === false) {
