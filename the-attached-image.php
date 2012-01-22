c5d42827eec095b302aab196eebe8f565e5dd092
veneficusunus <veneficusunus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-22 23:50:29 +0000

    WordPress version compat bump.
    git-svn-id: https://plugins.svn.wordpress.org/the-attached-image/trunk@493656 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- the-attached-image.php
+++ the-attached-image.php
@@ -3,7 +3,7 @@
 Plugin Name: The Attached Image
 Plugin URI: http://return-true.com/2008/12/wordpress-plugin-the-attached-image/
 Description: Display the first image attached to a post. Use the_attached_image() in the post loop. Order can be changed using menu order via the WP gallery. Based on the post image WordPress plugin by Kaf Oseo.
-Version: 2.6.1
+Version: 2.6.2
 Author: Paul Robinson
 ToDo: Massive code cleanup, basically clean up the code comment it alot & stuff planned for version 2.7.
 Author URI: http://return-true.com
